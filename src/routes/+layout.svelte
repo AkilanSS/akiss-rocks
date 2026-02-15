6b448fee92d3b7902329723f864bf9694f4f9b04
AkilanSS <akilanss22@gmail.com>
2026-02-15 13:26:29 +0530

    - added assets - added name with background overlay - added seperate style.css for landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
-
+	import './style.css';
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<link rel="stylesheet" href="style.css">
+	<title>Akiss~~</title>
 </svelte:head>
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,40 @@
-<h1>Akiss-Rocks goes here VVV</h1>
+<script lang="ts">
+
+</script>
+
+<div id="main-ctn">
+    <div id="header">
+        <div id="name-ctn">
+            <div id="name-sub-ctn">
+                <span id="name">Akilan</span>
+                <img src="src/lib/assets/vectors/flower-big.svg" alt="">
+            </div>
+            <span id="bio-line">I do cool stuff, occasionally</span>
+            <a class="l-logo" href="http://github.com/AkilanSS/" target="_blank"><img src="src/lib/assets/vectors/github.svg" alt=""></a>
+            <a class="l-logo" id="linkedin-logo" href="http://linkedin.com/in/akilanss" target="_blank"><img src="src/lib/assets/vectors/linkedin.svg" alt=""></a>
+        </div>
+        <div id="status-ctn">
+
+        </div>
+    </div>
+    <div id="nav">
+
+    </div>
+    <div id="recent-project-ctn">
+
+    </div>
+    <div id="recent-post-ctn" class="light-ctn">
+
+    </div>
+    <div class="light-ctn" id="spotlight-ctn">
+
+    </div>
+    <div id="gallery-ctn" class="light-ctn">
+
+    </div>
+    <div id="footer">
+
+    </div>
+</div>
+
+
