b2d94d78739d4ba4ed1893a3739501faa41d719b
AkilanSS <akilanss22@gmail.com>
2026-04-14 21:47:16 +0530

    - filled placeholder for dialog - made header more responsive

--- src/routes/about_me_dialog.svelte
+++ src/routes/about_me_dialog.svelte
@@ -5,5 +5,20 @@
 
 
 <dialog bind:this={dialog} closedby=any>
-    <p>Peak</p>
+    <div id="changelog-ctn">
+        <span id="changelog-head">Changelogs</span>
+        <span id="changelog-text">Here I jot down what I've been doing recently. Think of this as a relevant and dynamic alternative to the 'About Me' pages you see - adopted from <a href="https://nownownow.com/">https://nownownow.com/</a></span>
+    </div>
+    <div class="log-ctn">
+        <span class="log-date">Apr 14, 2026</span>
+        <span class="log-text"></span>
+    </div>
+        <div class="log-ctn">
+        <span class="log-date">Apr 14, 2026</span>
+        <span class="log-text"></span>
+    </div>
+        <div class="log-ctn">
+        <span class="log-date">Apr 14, 2026</span>
+        <span class="log-text"></span>
+    </div>
 </dialog>
